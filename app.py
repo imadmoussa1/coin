6a910f6bb9c8a9b15da3aaa86e63682b10814708
Imad Moussa <imadmoussa1@gmail.com>
2018-07-14 22:38:25 +0300

    Add the consensus method to check wich chain is valid

--- app.py
+++ app.py
@@ -45,6 +45,7 @@ def new_transaction():
 @app.route('/chain', methods=['GET'])
 def get_chain():
     # make sure we've the longest chain
+    consensus()
     chain_data = []
     for block in blockchain.chain:
         chain_data.append(block.__dict__)
@@ -60,6 +61,57 @@ def mine_unconfirmed_transactions():
     blockchain.add_new_transaction(transaction.json())
     result = blockchain.mine(peers)
     return jsonify({'message': "Block #{} is mined.".format(result)})
+
+
+# endpoint to add new peers to the network.
+@app.route('/add_nodes', methods=['POST'])
+def register_new_peers():
+    parser = reqparse.RequestParser()
+    parser.add_argument('node_url', type=str, required=True, help="Invalid transaction, node_url is missing")
+    data = parser.parse_args()
+    nodes = data['node_url']
+    if not nodes:
+        return jsonify({'message': "Invalid data"}), 400
+
+    peers.add(nodes)
+    return jsonify({'message': "Success"}), 200
+
+
+@app.route('/add_block', methods=['POST'])
+def validate_and_add_block():
+    block_data = request.get_json()
+    block = BlockModel(index=block_data["index"], transactions=block_data["transactions"],
+                       timestamp=block_data["timestamp"],
+                       previous_hash=block_data["previous_hash"])
+    block.hash = block_data['hash']
+    proof = block_data['nonce']
+    added = blockchain.add_block(block, proof)
+
+    if not added:
+        return "The block was discarded by the node", 400
+
+    return "Block added to the chain", 201
+
+
+def consensus():
+    # Our simple consnsus algorithm. If a longer valid chain is found, our chain is replaced with it.
+    global blockchain
+
+    longest_chain = None
+    current_len = len(blockchain.chain)
+    for node in peers:
+        response = requests.get('http://{}/chain'.format(node))
+        length = response.json()['length']
+        chain = response.json()['chain']
+        if length > current_len:
+            current_len = length
+            longest_chain = chain
+
+    if longest_chain:
+        blockchain.chain = BlockchainModel.convert_chain_from_json_to_object(chain)
+        return True
+
+    return False
 
 
 if __name__ == '__main__':
