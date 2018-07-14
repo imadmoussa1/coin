f0b6b2b57aa42ccf25cadbdf66e0f87c7153c416
Imad Moussa <imadmoussa1@gmail.com>
2018-07-14 15:18:52 +0300

    Add the method need to mine the blockchain with pow algo

--- app.py
+++ app.py
@@ -8,19 +8,53 @@ app = Flask(__name__)
 
 # the node's copy of blockchain
 blockchain = BlockchainModel()
+coin_nb = None
+transaction_amount = []
+
 
 # endpoint to submit a new transaction.
-@app.route('/new_transaction', methods=['POST'])
+@app.route('/initial_coin', methods=['GET'])
+def initial_coin():
+    global coin_nb
+    if coin_nb is None:
+        coin_nb = 10000
+        return json.dumps({'message': "nb of coin set : 10000"}), 200
+    return jsonify({'message': "can not change the number of coins"}), 400
+
+
+# endpoint to submit a new transaction.
+@app.route('/transaction', methods=['POST'])
 def new_transaction():
     parser = reqparse.RequestParser()
     parser.add_argument('address_from', type=str, required=True, help="Invalid transaction, address_from is missing")
     parser.add_argument('address_to', type=str, required=True, help="Invalid transaction, address_to is missing ")
     parser.add_argument('amount', type=int, required=True, help="Invalid transaction, amount is missing")
     data = parser.parse_args()
-
+    transaction_amount.append(data['amount'])
     transaction = TransactionModel(**data)
     blockchain.add_new_transaction(transaction.json())
-    return json.dumps(transaction.json()), 201
+    return jsonify(transaction.json()), 201
+
+
+# endpoint to return the chains in the block.
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    # make sure we've the longest chain
+    chain_data = []
+    for block in blockchain.chain:
+        chain_data.append(block.__dict__)
+    return jsonify({"length": len(chain_data), "chain": chain_data}), 200
+
+
+# endpoint to request the node to mine the unconfirmed transactions.
+@app.route('/mine', methods=['GET'])
+def mine_unconfirmed_transactions():
+    if not blockchain.unconfirmed_transactions:
+        return jsonify({'message': "No transactions to mine"})
+    transaction = TransactionModel("network", "miner", 1)
+    blockchain.add_new_transaction(transaction.json())
+    result = blockchain.mine()
+    return jsonify({'message': "Block #{} is mined.".format(result)})
 
 
 if __name__ == '__main__':
--- models/blockchain.py
+++ models/blockchain.py
@@ -19,3 +19,45 @@ class BlockchainModel:
 
     def add_new_transaction(self, transaction):
         self.unconfirmed_transactions.append(transaction)
+
+    def pow_definition(self, new_nonce):
+        # the pow condition to generate the proof
+        return new_nonce % BlockchainModel.difficulty == 0 and new_nonce % len(json.dumps(self.last_block.__dict__, sort_keys=True)) == 0
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    def mine(self):
+        # This function to add the pendingtransactions to the blockchain by adding them to the block
+        last_block = self.last_block
+
+        new_block = BlockModel(index=last_block.index + 1,
+                               transactions=self.unconfirmed_transactions,
+                               timestamp=time.time(),
+                               previous_hash=last_block.hash)
+
+        proof = self.proof_of_work(new_block)
+        self.add_block(new_block, proof)
+
+        self.unconfirmed_transactions = []
+        return new_block.index
+
+    def proof_of_work(self, block):
+        # proof of work algo
+        new_nonce = self.last_block.nonce
+
+        while not self.pow_definition(new_nonce):
+            new_nonce += 1
+        return new_nonce
+
+    def add_block(self, block, proof):
+        # A function that adds the block to the chain
+        previous_hash = self.last_block.hash
+
+        if previous_hash != block.previous_hash:
+            return False
+
+        block.nonce = proof
+        self.chain.append(block)
+        return True
